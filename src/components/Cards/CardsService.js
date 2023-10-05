a060033db37a97f305e6d21ec1cbd41f3ffa34b5
rafikaauda <rafika.auda4@gmail.com>
2023-10-06 03:00:34 +0700

    add service ui and handler

---
+++ src/components/Cards/CardsService.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getService } from '../../feature/service/ServiceSlice';
+
+export default function CardsService() {
+  const dispatch = useDispatch();
+  const service = useSelector((state) => state.service.service);
+  useEffect(() => {
+    dispatch(getService());
+  }, [dispatch]);
+  return (
+    <section className='2xl:px-52 lg:px-20 md:px-20 sm:px-24 px-5'>
+      <div className='grid 2xl:gap-8 lg:gap-24 md:gap-20 sm:gap-28 gap-10 2xl:grid-cols-7 lg:grid-cols-5 md:grid-cols-3 grid-cols-2 justify-center'>
+        {service.map((item) => (
+          <div
+            className='card w-44 bg-slate-100 cursor-pointer'
+            key={item.service_code}>
+            <figure>
+              <img
+                src={item.service_icon}
+                alt='Shoes'
+              />
+            </figure>
+            <div className='flex justify-center'>
+              <h2 className='card-title text-sm text-center'>
+                {item.service_name}
+              </h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
